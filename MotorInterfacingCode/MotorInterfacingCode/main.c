dcee8e30682014f4d2dce7033daeeb524d13331e
Selim <selimchemutai19@gmail.com>
2021-11-09 15:28:14 +0300

    Create main.c

---
+++ MotorInterfacingCode/MotorInterfacingCode/main.c
@@ -0,0 +1,18 @@
+/*
+ * MotorInterfacingCode.c
+ *
+ * Created: 09/11/2021 15:26:53
+ * Author : Default
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
