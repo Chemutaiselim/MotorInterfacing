9865a7ffc70839d828370eabc676f5a2659f9ca2
Selim <selimchemutai19@gmail.com>
2021-11-09 15:32:38 +0300

    Update DCMotorCode.h

--- MotorInterfacingCode/MotorInterfacingCode/DCMotorCode.h
+++ MotorInterfacingCode/MotorInterfacingCode/DCMotorCode.h
@@ -2,14 +2,15 @@
  * DCMotorCode.h
  *
  * Created: 09/11/2021 15:29:13
- *  Author: Default
+ *  Author: Pamela Chemutai 
  */ 
 
 
 #ifndef DCMOTORCODE_H_
 #define DCMOTORCODE_H_
 
-
+void ADC_Init(); /* ADC Initialization function */ 
+int ADC_Read(char channel); /* ADC Read function */
 
 
 
